02c107adb6bef206fe803136418039233d936b3d
Dave Calkins <dave.calkins@hexagon.com>
2022-11-01 19:55:03 -0400

    wired up basic MVVM

---
+++ src/bqscoreboard/BQScores.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bqscoreboard
+{
+    internal class BQScores
+    {
+        public string Greeting = "Hello, quizzers!";
+    }
+}
---
+++ src/bqscoreboard/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bqscoreboard
+{
+    internal class MainPageViewModel : ViewModelBase
+    {
+        public string Greeting
+        {
+            get => model.Greeting;
+            set => SetProperty(ref model.Greeting, value);
+        }
+
+        private BQScores model = new();
+
+        public void Initialize()
+        {
+        }
+    }
+}
